a57dcc7faa8d05ed0ff356adb6cfde2f96e80e54
Will Fenton <willfenton14@gmail.com>
2020-03-16 17:16:02 -0600

    Added untracked files

---
+++ script.js
@@ -0,0 +1,23 @@
+var vm = new Vue({
+    el: "#vue-app",
+    data: function () {
+        return {
+            projects_url: "data/projects.json",
+            projects: []
+        }
+    },
+    created() {
+        this.fetchProjects();
+    },
+    methods: {
+        fetchProjects: function() {
+            console.log(this.projects_url);
+            fetch(this.projects_url)
+                .then(response => response.json())
+                .then(json => {
+                    console.log(json);
+                    this.projects = json["projects"];
+                });
+        }
+    }
+})
